39cc076e642b05f06d982051d2344044dd2145d9
PhucHau1609 <166683665+PhucHau1609@users.noreply.github.com>
2024-11-29 23:33:16 +0700

    Done 35 36
    finish beta game version

---
+++ Assets/Scripts/DayNightSystem.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DayNightSystem : MonoBehaviour
+{
+    public Light directionalLight;
+
+    public float dayDurationInSeconds = 24f;
+    public int currentHour;
+    float currentTimeOfDay = 0.35f;
+
+    public List<SkyBoxTimeMapping> timeMappings;
+
+    float blenderValue = 0.0f;
+
+    bool lockNextDayTrigger = false;
+
+
+    [Header("UI Day Time")]
+    public TextMeshProUGUI timeUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTimeOfDay += Time.deltaTime / dayDurationInSeconds;
+        currentTimeOfDay %= 1;
+
+        currentHour = Mathf.FloorToInt(currentTimeOfDay * 24);
+
+        timeUI.text = $"{currentHour}:00";
+
+        directionalLight.transform.rotation = Quaternion.Euler(new Vector3((currentTimeOfDay * 360) - 90, 170, 0));
+
+
+        UpdateSkybox();
+    }
+
+    private void UpdateSkybox()
+    {
+        Material currentSkybox = null;
+        foreach(SkyBoxTimeMapping mapping in timeMappings)
+        {
+            if(currentHour == mapping.hour)
+            {
+                currentSkybox = mapping.skyboxMaterials;
+
+                if(currentSkybox.shader != null)
+                {
+                    if (currentSkybox.shader.name == "Custom/SkyboxTransition")
+                    {
+                        blenderValue += Time.deltaTime;
+                        blenderValue = Mathf.Clamp01(blenderValue);
+
+                        currentSkybox.SetFloat("_TransitionFactor",blenderValue);
+                    }
+
+                    else
+                    {
+                        blenderValue = 0.0f;
+                    }
+                }
+
+                break;
+            }
+        }
+
+
+        if(currentHour == 0 && lockNextDayTrigger == false)
+        {
+            TimeManager.Instance.TriggerNextDay();
+            lockNextDayTrigger = true;
+        }
+
+        if(currentHour != 0)
+        {
+            lockNextDayTrigger = false;
+        }
+
+        
+        if(currentSkybox != null)
+        {
+            RenderSettings.skybox = currentSkybox;
+        }
+    }
+}
+
+[System.Serializable]
+public class SkyBoxTimeMapping
+{
+    public string phaseName;
+    public int hour;
+    public Material skyboxMaterials;
+}
